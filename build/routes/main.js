27b2c0bd917b0982398ede48b5901180da44e4c5
Joel Sequeira <joelseq96@gmail.com>
2016-06-29 02:15:10 +0400

    RESTful routes setup

--- build/routes/main.js
+++ build/routes/main.js
@@ -2,10 +2,68 @@
 
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-/* GET users listing. */
-router.get('/hello', function (req, res, next) {
-  res.json({ hello: 'world' });
+/* Get all users */
+router.get('/users', function (req, res) {
+  User.find({}, function (err, users) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Something went wrong." });
+    }
+    res.send(users);
+  });
+});
+
+/* Save a user */
+router.post('/users', function (req, res) {
+  if (!req.body.fname || !req.body.lname) {
+    return res.status(400).json({ error: "Firstname and Lastname required." });
+  }
+
+  var user = {
+    firstName: req.body.fname,
+    lastName: req.body.lname
+  };
+
+  User.create(user, function (err, savedUser) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Something went wrong." });
+    }
+    res.status(200).json({ message: "User successfully created." });
+  });
+});
+
+/* Update a user */
+router.put('/users/:id', function (req, res) {
+  if (!req.body.fname || !req.body.lname) {
+    return res.status(400).json({ error: "Firstname and Lastname required." });
+  }
+
+  var user = {
+    firstName: req.body.fname,
+    lastName: req.body.lname
+  };
+
+  User.findByIdAndUpdate(req.params.id, user, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Something went wrong." });
+    }
+    res.status(200).json({ message: "Successfully updated user." });
+  });
+});
+
+/* Delete a user */
+router.delete('/users/:id', function (req, res) {
+  User.findByIdAndRemove(req.params.id, function (err) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Something went wrong." });
+    }
+    res.status(200).json({ message: "User successfully removed." });
+  });
 });
 
 module.exports = router;
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,9 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-/* GET users listing. */
-router.get('/hello', function(req, res, next) {
-  res.json({hello: 'world'});
+/* Get all users */
+router.get('/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if(err) {
+      console.log(err);
+      return res.status(500).json({message: "Something went wrong."});
+    }
+    res.send(users);
+  });
+});
+
+/* Save a user */
+router.post('/users', (req, res) => {
+  if(!req.body.fname || !req.body.lname) {
+    return res.status(400).json({error: "Firstname and Lastname required."});
+  }
+
+  let user = {
+    firstName: req.body.fname,
+    lastName: req.body.lname
+  };
+
+  User.create(user, (err, savedUser) => {
+    if(err) {
+      console.log(err);
+      return res.status(500).json({message: "Something went wrong."});
+    }
+    res.status(200).json({message: "User successfully created."});
+  });
+});
+
+/* Update a user */
+router.put('/users/:id', (req, res) => {
+  if(!req.body.fname || !req.body.lname) {
+    return res.status(400).json({error: "Firstname and Lastname required."});
+  }
+
+  let user = {
+    firstName: req.body.fname,
+    lastName: req.body.lname
+  };
+
+  User.findByIdAndUpdate(req.params.id, user, (err, foundUser) => {
+    if(err) {
+      console.log(err);
+      return res.status(500).json({message: "Something went wrong."});
+    }
+    res.status(200).json({message: "Successfully updated user."});
+  });
+});
+
+/* Delete a user */
+router.delete('/users/:id', (req, res) => {
+  User.findByIdAndRemove(req.params.id, (err) => {
+    if(err) {
+      console.log(err);
+      return res.status(500).json({message: "Something went wrong."});
+    }
+    res.status(200).json({message: "User successfully removed."});
+  });
 });
 
 module.exports = router;
